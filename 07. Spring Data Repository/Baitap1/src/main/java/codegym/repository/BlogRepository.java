b69fd62a384e7953447fa43edb55c664d46fb345
lehoanglong <lelong.ll32@gmail.com>
2021-10-09 17:56:29 +0700

    Spring Data Repository

---
+++ 07. Spring Data Repository/Baitap1/src/main/java/codegym/repository/BlogRepository.java
@@ -0,0 +1,21 @@
+package codegym.repository;
+
+import codegym.model.Blog;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+@Repository
+public interface BlogRepository extends CrudRepository<Blog,Integer> {
+
+    Page<Blog> findAllByTitleContaining(String name,Pageable pageable);
+
+    Page<Blog> findAll(Pageable pageable);
+
+    @Query(value = "SELECT * FROM blog WHERE category_id = :id",nativeQuery = true)
+    Page<Blog> findAllByCategory(Pageable pageable,@Param("id") int id);
+}
